3835fc395de20b87ee5d59627615d0f4cab86b91
AnyaNek0 <16bitstreams@gmail.com>
2022-11-08 00:47:24 +0500

    test: added more tests to calculations

--- SalaryCalculation.Tests/CalcualtionsTests.cs
+++ SalaryCalculation.Tests/CalcualtionsTests.cs
@@ -20,16 +20,17 @@ namespace SalaryCalculation.Tests
 
             metrics.CalculateMetrics(0.15, 15279, 0.13);
 
-            Assert.Equal(20000, metrics.Salary);
-            Assert.Equal(230.45, metrics.HourCostFact);
-            Assert.Equal(125.0, metrics.HourCostHand);
-            Assert.Equal(54133.33, metrics.Income);
-            Assert.Equal(31187.85, metrics.Expenses);
-            Assert.Equal(22945.48, metrics.Profit);
-            Assert.Equal(42.39, metrics.ProfitAbility);
-            Assert.Equal(23000, metrics.SalaryBeforeTax);
-            Assert.Equal(20010, metrics.SalaryAftertax);
+            Assert.Equal(20000, Math.Round(metrics.Salary,2));
+            Assert.Equal(230.45, Math.Round(metrics.HourCostFact,2));
+            Assert.Equal(125.0, Math.Round(metrics.HourCostHand,2));
+            Assert.Equal(54133.33, Math.Round(metrics.Income,2));
+            Assert.Equal(31187.85, Math.Round(metrics.Expenses,2));
+            Assert.Equal(22945.48, Math.Round(metrics.Profit,2));
+            Assert.Equal(42.39, Math.Round(metrics.ProfitAbility,2));
+            Assert.Equal(23000, Math.Round(metrics.SalaryBeforeTax,2));
+            Assert.Equal(20010, Math.Round(metrics.SalaryAftertax,2));
         }
+
         [Fact]
         public void TestMetrics2()
         {
@@ -45,15 +46,67 @@ namespace SalaryCalculation.Tests
 
             metrics.CalculateMetrics(0.15, 15279, 0.13);
 
-            Assert.Equal(10000, metrics.Salary);
-            Assert.Equal(119.26, metrics.HourCostFact);
-            Assert.Equal(62.50, metrics.HourCostHand);
-            Assert.Equal(27066.67, metrics.Income);
-            Assert.Equal(16139.85, metrics.Expenses);
-            Assert.Equal(10926.82, metrics.Profit);
-            Assert.Equal(40.37, metrics.ProfitAbility);
-            Assert.Equal(11500, metrics.SalaryBeforeTax);
-            Assert.Equal(10005, metrics.SalaryAftertax);
+            Assert.Equal(10000, Math.Round(metrics.Salary,2));
+            Assert.Equal(119.26, Math.Round(metrics.HourCostFact,2));
+            Assert.Equal(62.50, Math.Round(metrics.HourCostHand,2));
+            Assert.Equal(27066.67, Math.Round(metrics.Income,2));
+            Assert.Equal(16139.85, Math.Round(metrics.Expenses,2));
+            Assert.Equal(10926.82, Math.Round(metrics.Profit,2));
+            Assert.Equal(40.37, Math.Round(metrics.ProfitAbility,2));
+            Assert.Equal(11500, Math.Round(metrics.SalaryBeforeTax,2));
+            Assert.Equal(10005, Math.Round(metrics.SalaryAftertax,2));
+        }
+
+        [Fact]
+        public void TestMetrics3()
+        {
+            var employeeId = 1;
+            var ratePerHour = 400.00;
+            var fullSalary = 1.00;
+            double employmentType = 1.0;
+
+            EmployeeFinancialMetrics metrics = new EmployeeFinancialMetrics(employeeId,
+                ratePerHour,
+                fullSalary,
+                employmentType, true);
+
+            metrics.CalculateMetrics(0.15, 15279, 0.13);
+
+            Assert.Equal(1, Math.Round(metrics.Salary,2));
+            Assert.Equal(34.68, Math.Round(metrics.HourCostFact,2));
+            Assert.Equal(0.01, Math.Round(metrics.HourCostHand, 2));
+            Assert.Equal(54133.33, Math.Round(metrics.Income, 2));
+            Assert.Equal(4693.17, Math.Round(metrics.Expenses,2));
+            Assert.Equal(49440.16, Math.Round(metrics.Profit,2));
+            Assert.Equal(91.33, Math.Round(metrics.ProfitAbility,2));
+            Assert.Equal(1.15, Math.Round(metrics.SalaryBeforeTax,2));
+            Assert.Equal(1, Math.Round(metrics.SalaryAftertax,2));
+        }
+
+        [Fact]
+        public void TestMetrics4()
+        {
+            var employeeId = 1;
+            var ratePerHour = 400.00;
+            var fullSalary = 0;
+            double employmentType = 1.0;
+
+            EmployeeFinancialMetrics metrics = new EmployeeFinancialMetrics(employeeId,
+                ratePerHour,
+                fullSalary,
+                employmentType, true);
+
+            metrics.CalculateMetrics(0.15, 15279, 0.13);
+
+            Assert.Equal(0, Math.Round(metrics.Salary, 2));
+            Assert.Equal(34.67, Math.Round(metrics.HourCostFact, 2));
+            Assert.Equal(0, Math.Round(metrics.HourCostHand, 2));
+            Assert.Equal(54133.33, Math.Round(metrics.Income, 2));
+            Assert.Equal(4691.85, Math.Round(metrics.Expenses, 2));
+            Assert.Equal(49441.48, Math.Round(metrics.Profit, 2));
+            Assert.Equal(91.33, Math.Round(metrics.ProfitAbility, 2));
+            Assert.Equal(0, Math.Round(metrics.SalaryBeforeTax, 2));
+            Assert.Equal(0, Math.Round(metrics.SalaryAftertax, 2));
         }
     }
 }
